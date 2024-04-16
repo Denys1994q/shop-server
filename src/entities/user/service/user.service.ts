1eb472aeee514415853c02a9ed9d6edffe0eb1cf
Denys1994q <drybachok@gmail.com>
2024-04-16 17:21:17 +0300

    add auth entity, add nest jwt

--- src/entities/user/service/user.service.ts
+++ src/entities/user/service/user.service.ts
@@ -1,24 +1,20 @@
-import {Injectable, HttpException, HttpStatus} from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import {UserDocument} from '../model/user.schema';
 import {InjectModel} from '@nestjs/mongoose';
 import {Model} from 'mongoose';
-import {statusMessages} from '@app/constants/errorMessages.constant';
 import * as bcrypt from 'bcrypt';
-import * as jwt from 'jsonwebtoken';
 import {CreateUserDto} from '../dto/createUserDto';
-
-export type UserWithoutPasswordHash = Omit<UserDocument, 'passwordHash'>;
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel('User') private userModel: Model<UserDocument>) {}
 
-  async createUser(userData: CreateUserDto): Promise<{user: UserWithoutPasswordHash; token: string}> {
+  async findOne(email: string): Promise<UserDocument> {
+    return this.userModel.findOne({email: email});
+  }
+
+  async createUser(userData: CreateUserDto): Promise<UserDocument> {
     const {firstName, lastName, email, phoneNumber, password} = userData;
-    const existingUser = await this.userModel.findOne({email: email});
-    if (existingUser) {
-      throw new HttpException(statusMessages.USER_ALREADY_EXISTS, HttpStatus.CONFLICT);
-    }
     const saltOrRounds = 10;
     const hash = await bcrypt.hash(password, saltOrRounds);
     const newUser = new this.userModel({
@@ -29,9 +25,6 @@ export class UserService {
       passwordHash: hash
     });
     const savedUser = await newUser.save();
-    const token = jwt.sign({_id: savedUser._id}, process.env.JWT_SECRET_KEY, {expiresIn: '24h'});
-    const {passwordHash, ...data} = savedUser.toJSON();
-
-    return {user: data, token};
+    return savedUser;
   }
 }
--- src/entities/user/user.module.ts
+++ src/entities/user/user.module.ts
@@ -1,12 +1,11 @@
 import {Module} from '@nestjs/common';
-import {UserController} from './controller/user.controller';
 import {MongooseModule} from '@nestjs/mongoose';
 import {UserSchema} from './model/user.schema';
 import {UserService} from './service/user.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{name: 'User', schema: UserSchema}])],
-  controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService]
 })
 export class UserModule {}
